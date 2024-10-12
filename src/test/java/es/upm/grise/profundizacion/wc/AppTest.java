51ee34b62bd454e575e59e38a71bb7b32c0e468a
miguel <miguel.hernando.padilla@alumnos.upm.es>
2024-10-12 18:01:12 +0200

    Añadidos los imports de la clase de AppTest

--- src/test/java/es/upm/grise/profundizacion/wc/AppTest.java
+++ src/test/java/es/upm/grise/profundizacion/wc/AppTest.java
@@ -1,4 +1,15 @@
 package es.upm.grise.profundizacion.wc;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+
 public class AppTest {
     
 }
