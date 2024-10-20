5c57c1108e3b1855a0c0bcb6f1f0bb9b1722fd4f
Julián C <julian.gcastro@alumnos.upm.es>
2024-10-20 14:05:18 +0200

    Add more test to test some edge cases.

--- src/test/java/es/upm/alumnos/profundizacion/wc/CounterTest.java
+++ src/test/java/es/upm/alumnos/profundizacion/wc/CounterTest.java
@@ -7,23 +7,24 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import es.upm.grise.profundizacion.wc.Counter;
 
 public class CounterTest {
 
-    private static final String TEST_TEXT = " \thello\n\n\n this is a test text\n\t", BLANK = " ";
+    private static final String TEST_TEXT = " \thello\n\n\n this is a test text\n\t",
+            BLANK = " ",
+            ONE_WORD = "word",
+            TWO_WORDS = " one two",
+            GERMAN_WORDS = "Fußball, Eselsbrücke und Fremdschämen ",
+            LINES = "Hello     Richard Feynman!\n",
+            LINES1 = "\nHej     Søren Kierkegaard!";
+
 
     static BufferedReader latestBufferedReader;
 
@@ -34,32 +35,32 @@ public class CounterTest {
 
     @ParameterizedTest
     @EmptySource
-    @ValueSource(strings = {TEST_TEXT, BLANK})
+    @ValueSource(strings = {TEST_TEXT, BLANK, ONE_WORD, TWO_WORDS, GERMAN_WORDS, LINES, LINES1})
     public void getNumberCharactersTest(String testedString) throws IOException {
         Counter counter = createCounter(testedString);
-        assertEquals(counter.getNumberCharacters(), testedString.length());
+        assertEquals(testedString.length(), counter.getNumberCharacters(), "Not correct count of characters for input <"+testedString+">");
     }
 
     @ParameterizedTest
     @EmptySource
-    @ValueSource(strings = {TEST_TEXT, BLANK})
+    @ValueSource(strings = {TEST_TEXT, BLANK, ONE_WORD, TWO_WORDS, GERMAN_WORDS, LINES, LINES1})
     public void getNumberWordsTest(String testedString) throws IOException {
         Counter counter = createCounter(testedString);
         if ( ! testedString.isBlank())
-            assertEquals(counter.getNumberWords(), testedString.trim().split("\\s+").length);
+            assertEquals(testedString.trim().split("\\s+").length, counter.getNumberWords(), "Not correct count of words for input <"+testedString+">");
         else
-            assertEquals(counter.getNumberWords(), 0);
+            assertEquals(0, counter.getNumberWords());
     }
 
     @ParameterizedTest 
     @EmptySource
-    @ValueSource(strings = {TEST_TEXT, BLANK})
+    @ValueSource(strings = {TEST_TEXT, BLANK, ONE_WORD, TWO_WORDS, GERMAN_WORDS, LINES, LINES1})
     public void getNumberLines(String testedString) throws IOException {
         Counter counter = createCounter(testedString);
         Matcher m = Pattern.compile("\r\n|\r|\n").matcher(testedString);
         int lines = 0;
         while (m.find()){lines ++;}
-        assertEquals(counter.getNumberLines(), lines);
+        assertEquals(lines, counter.getNumberLines(), "Not correct count of words for lines <"+testedString+">");
     }
 
     @AfterEach
