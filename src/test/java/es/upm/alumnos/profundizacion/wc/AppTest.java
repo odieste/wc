5903deb162db123df2a8a5c32434c3cb33d6c430
Dariagan <seomasini@gmail.com>
2024-10-20 11:41:42 +0200

    windows fix for testAnEmptyFile()

--- src/test/java/es/upm/alumnos/profundizacion/wc/AppTest.java
+++ src/test/java/es/upm/alumnos/profundizacion/wc/AppTest.java
@@ -6,38 +6,17 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
-
-import org.junit.jupiter.api.BeforeAll;
+import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.io.TempDir;
 
 import es.upm.grise.profundizacion.wc.App;
 import uk.org.webcompere.systemstubs.jupiter.SystemStub;
 import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 import uk.org.webcompere.systemstubs.stream.SystemOut;
 
-/*
-Documentación de apoyo
-- https://github.com/webcompere/system-stubs/blob/main/system-stubs-jupiter/README.md
-Ejemplo para implementar el tema que habló el profesor del Sistem.exit()
-- https://github.com/webcompere/system-stubs/blob/main/system-stubs-jupiter/src/test/java/uk/org/webcompere/systemstubs/jupiter/examples/SystemExitUseCase.java
-Ejemplo para implementar el revisar la salida del programa
-- https://www.baeldung.com/java-system-stubs#2-junit-5-example
-*/
-
 @ExtendWith(SystemStubsExtension.class)
 public class AppTest {
-
-    @TempDir
-    private static File dir;
-    private static File emptyFile;
-
-    @BeforeAll
-    public static void initFiles() throws IOException {
-        emptyFile = new File(dir, "emptyFile.txt");
-        emptyFile.createNewFile();
-    }
 
     private static void callApp(String... args) {
         App.main(args);
@@ -78,12 +57,27 @@ public class AppTest {
     }
 
     @Test
-    public void testAnEmptyFile() {
-        final String filename = emptyFile.getAbsolutePath();
-        callApp("-cwl", filename);
-        assertLinesMatch(List.of("^.*0.*0.*0.*"), getOutLines(),
-                "The program should report zeros for all counts on an empty file!");
-        assertLinesMatch(List.of(".*" + filename), getOutLines(),
-                "The program should report the name of the file that had been processed!");
+    public void testAnEmptyFile() throws IOException {
+        File emptyFile = File.createTempFile("empty", null);
+        emptyFile.deleteOnExit();  
+
+        Path normalizedPath = emptyFile.toPath();
+        String normalizedFilename = normalizedPath.toString();
+
+        callApp("-cwl", normalizedFilename);
+
+        assertAll(
+            () -> assertLinesMatch(
+                List.of("^.*0.*0.*0.*"), 
+                getOutLines(),
+                "The program should report zeros for all counts on an empty file!"
+            ),
+            () -> assertLinesMatch(
+                List.of(".*" + Pattern.quote(normalizedFilename)), 
+                getOutLines(),
+                "The program should report the name of the file that had been processed!"
+            )
+        );
     }
+
 }
