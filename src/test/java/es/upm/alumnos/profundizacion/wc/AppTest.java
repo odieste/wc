e3949dd1e7b8357d905a423e1834560bd1e5b959
Julián C <julian.gcastro@alumnos.upm.es>
2024-10-20 16:30:36 +0200

    Add a workaround to make TempDir to work on Windows

--- src/test/java/es/upm/alumnos/profundizacion/wc/AppTest.java
+++ src/test/java/es/upm/alumnos/profundizacion/wc/AppTest.java
@@ -2,15 +2,19 @@ package es.upm.alumnos.profundizacion.wc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.regex.Pattern;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.api.condition.OS;
 
 import es.upm.grise.profundizacion.wc.App;
 import uk.org.webcompere.systemstubs.jupiter.SystemStub;
@@ -19,27 +23,33 @@ import uk.org.webcompere.systemstubs.stream.SystemOut;
 
 @ExtendWith(SystemStubsExtension.class)
 public class AppTest {
-    
+
 	@TempDir
-    private static File dir;
-    private static File emptyFile;
-    private static File notAllowed;
-    private static File quotes;
+    private static Path dir;
+    private static Path emptyFile;
+    private static Path quotes;
     
     @BeforeAll
-    public static void initFiles() throws IOException {
-        emptyFile = new File(dir, "emptyFile.txt");
-        emptyFile.createNewFile();
-        notAllowed = new File(dir, "you_shall_not_pass.txt");
-        notAllowed.createNewFile();
-        notAllowed.setReadable(false);
-        quotes = new File(dir, "quotes.txt");
-        Files.write(quotes.toPath(),
+    private static void initFiles() throws IOException {
+        emptyFile = dir.resolve("emptyFile.txt");
+        Files.createFile(emptyFile);
+        quotes = dir.resolve("quotes.txt");
+        Files.write(quotes,
                 List.of("Without music, life would be a mistake. - Nietzsche",
                         "There's no point in being grown up if you can't act childish sometimes. - 4th Doctor",
                         "Forget the past. No one becomes successful in the past. - Anonymous")
                 );
     }
+    
+    /*
+     * Issues on Windows working with @TempDir
+     * https://github.com/junit-team/junit5/issues/2811
+     */
+    @AfterEach
+	@EnabledOnOs(OS.WINDOWS)
+	private void cleanUp() {
+		System.gc();
+	}
     
     private static void callApp(String... args) {
         App.main(args);
@@ -74,37 +84,31 @@ public class AppTest {
     public void testAnNonExistingFile() {
         final String filename = "no_exist_file.txt";
         callApp("-w", filename);
-        assertLinesMatch(List.of("Cannot find file.*"), getOutLines(),
+        final List<String> output = getOutLines();
+        assertLinesMatch(List.of("Cannot find file.*"), output,
                 "The program should report if the file cannot be found!");
-        assertLinesMatch(List.of(".*" + Pattern.quote(filename)), getOutLines(), "The program should report the name of the file!");
-    }
-
-    @Test
-    public void testPermissionsOnAFile() {
-        final String filename = notAllowed.getAbsolutePath();
-        callApp("-cwl", filename);
-        assertLinesMatch(List.of("Cannot find file.*"), getOutLines(),
-                "The program should report if the file cannot be accesible!");
-        assertLinesMatch(List.of(".*" + Pattern.quote(filename)), getOutLines(), "The program should report the name of the file!");
+        assertLinesMatch(List.of(".*" + Pattern.quote(filename)), output, "The program should report the name of the file!");
     }
 
     @Test
     public void testAnEmptyFile() {
-        final String filename = emptyFile.getAbsolutePath();
+        final String filename = emptyFile.toString();
         callApp("-cwl", filename);
-        assertLinesMatch(List.of("\\s\\d+\\s\\d+\\s\\d+.*"), getOutLines(),
+        final List<String> output = getOutLines();
+        assertLinesMatch(List.of("\\s\\d+\\s\\d+\\s\\d+.*"), output,
                 "The program should report as many counts requested!");
-        assertLinesMatch(List.of(".*" + Pattern.quote(filename)), getOutLines(),
+        assertLinesMatch(List.of(".*" + Pattern.quote(filename)), output,
                 "The program should report the name of the file that had been processed!");
     }
 
     @Test
     public void testAWrittenFileTwoOptions() {
-        final String filename = quotes.getAbsolutePath();
+        final String filename = quotes.toString();
         callApp("-cl", filename);
-        assertLinesMatch(List.of("\\s\\d+\\s\\d+.*"), getOutLines(),
+        final List<String> output = getOutLines();
+        assertLinesMatch(List.of("\\s\\d+\\s\\d+.*"), output,
                 "The program should report as many counts requested!");
-        assertLinesMatch(List.of(".*" + Pattern.quote(filename)), getOutLines(),
+        assertLinesMatch(List.of(".*" + Pattern.quote(filename)), output,
                 "The program should report the name of the file that had been processed!");
     }
 }
