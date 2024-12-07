19a269e97ef84653a2ca2e9c26bd39c673c79657
chainiDV <28576404+chainiDV@users.noreply.github.com>
2024-12-07 17:51:59 +0100

    Cerrar el lector del archivo correctamente al terminar la ejecucion

--- src/main/java/es/upm/grise/profundizacion/wc/App.java
+++ src/main/java/es/upm/grise/profundizacion/wc/App.java
@@ -22,52 +22,61 @@ public class App {
 
 		// Check the commands later (see switch below)
 		// At this point, we open the text file
-		BufferedReader br = null;
     	String fileName = args[1];
 
-		try{
-			br = new BufferedReader(new FileReader(fileName));
-		} catch(Exception e) {
-    		System.out.println("Cannot find file: " + fileName);
-    		return;
-		}
-		
-		// And count the characters, words and lines. It is easier
-		// to count everything than rewind the file several times,
-		// which is not an straightforward operation
-    	Counter counter = null;
-		try {
-			counter = new Counter(br);
-		} catch (IOException e) {
-    		System.out.println("Error reading file: " + fileName);
-    		return;
-		}
-		
-    	// Verify that the commands start with -. It is not necessary
-    	// because the first parameter is a command always, but...
-		String commands = args[0];
-		if(commands.charAt(0) != '-') {
-    		System.out.println("The commands do not start with -");
-    		return;
-		}
-		
-		// Check the commands and return values. In case of wrong commands
-		// the program exits with error
-		String result = "";
-		commands = commands.substring(1);
-		for(char c : commands.toCharArray()) {
-			switch(c) {
-			case 'c': result += "\t" + Integer.toString(counter.getNumberCharacters()); break;
-			case 'l': result += "\t" + Integer.toString(counter.getNumberLines()); break;
-			case 'w': result += "\t" + Integer.toString(counter.getNumberWords()); break;
-			default:
-	    		System.out.println("Unrecognized command: " + c);
+		try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
+			// And count the characters, words and lines. It is easier
+			// to count everything than rewind the file several times,
+			// which is not an straightforward operation
+	    	Counter counter = null;
+			try {
+				counter = new Counter(br);
+			} catch (IOException e) {
+	    		System.out.println("Error reading file: " + fileName);
 	    		return;
 			}
+			
+	    	// Verify that the commands start with -. It is not necessary
+	    	// because the first parameter is a command always, but...
+			String commands = args[0];
+			if(commands.charAt(0) != '-') {
+	    		System.out.println("The commands do not start with -");
+	    		return;
+			}
+			
+			// Check the commands and return values. In case of wrong commands
+			// the program exits with error
+			String result = "";
+			commands = commands.substring(1);
+			for(char c : commands.toCharArray()) {
+				switch(c) {
+				case 'c': result += "\t" + Integer.toString(counter.getNumberCharacters()); break;
+				case 'l': result += "\t" + Integer.toString(counter.getNumberLines()); break;
+				case 'w': result += "\t" + Integer.toString(counter.getNumberWords()); break;
+				default:
+		    		System.out.println("Unrecognized command: " + c);
+		    		try {
+						br.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		    		return;
+				}
+			}
+			
+			// Print the results
+			// The counts are returned in the specified order
+			System.out.println(result + "\t" + fileName);
+			
+			try {
+				br.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+		    System.out.println("Cannot find file: " + fileName);
 		}
-		
-		// Print the results
-		// The counts are returned in the specified order
-		System.out.println(result + "\t" + fileName);
 	}
 }
